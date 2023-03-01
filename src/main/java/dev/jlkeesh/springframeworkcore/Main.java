03238b0f220efc4ab457e3cf2e7ba62b65799931
pdp <john.lgd65@gmail.com>
2023-03-01 15:34:13 +0500

    core_xml_config

--- src/main/java/dev/jlkeesh/springframeworkcore/Main.java
+++ src/main/java/dev/jlkeesh/springframeworkcore/Main.java
@@ -1,14 +1,13 @@
 package dev.jlkeesh.springframeworkcore;
 
-import dev.jlkeesh.springframeworkcore.beans.MyBean;
 import dev.jlkeesh.springframeworkcore.beans.MyBean2;
-import dev.jlkeesh.springframeworkcore.config.MYConfig;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(MYConfig.class);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ic-config.xml");
+        /*context.setConfigLocation("ic-config.xml");
+        context.refresh();*/
         MyBean2 myBean = context.getBean(MyBean2.class);
         myBean.hi2();
     }
--- src/main/java/dev/jlkeesh/springframeworkcore/beans/MyBean2.java
+++ src/main/java/dev/jlkeesh/springframeworkcore/beans/MyBean2.java
@@ -3,9 +3,9 @@ package dev.jlkeesh.springframeworkcore.beans;
 public class MyBean2 {
     private MyBean myBean;
 
-    /*public MyBean2(MyBean myBean) { // CI
-        this.myBean = myBean;
-    }*/
+//    public MyBean2(MyBean myBean) { // CI
+//        this.myBean = myBean;
+//    }
 
     public void setMyBean(MyBean myBean) { // SI
         this.myBean = myBean;
