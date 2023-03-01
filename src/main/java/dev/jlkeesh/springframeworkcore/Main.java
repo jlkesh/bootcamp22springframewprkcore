e8d1b6270b01b5ac45243eddb23609fb9e3e74ca
pdp <john.lgd65@gmail.com>
2023-03-01 15:15:18 +0500

    initial

--- src/main/java/dev/jlkeesh/springframeworkcore/Main.java
+++ src/main/java/dev/jlkeesh/springframeworkcore/Main.java
@@ -1,17 +1,7 @@
 package dev.jlkeesh.springframeworkcore;
 
-import dev.jlkeesh.springframeworkcore.beans.MyBean;
-import dev.jlkeesh.springframeworkcore.beans.MyBean2;
-import dev.jlkeesh.springframeworkcore.config.MyConfig;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
-        MyBean2 myBean2 = (MyBean2) context.getBean("myBean2");
-        MyBean2 myBean2_2 = (MyBean2) context.getBean("myBean2");
-        System.out.println(System.identityHashCode(myBean2));
-        System.out.println(System.identityHashCode(myBean2_2));
+
     }
 }
--- src/main/java/dev/jlkeesh/springframeworkcore/config/MyConfig.java
+++
@@ -1,8 +0,0 @@
-package dev.jlkeesh.springframeworkcore.config;
-
-
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan(basePackages = "dev.jlkeesh.springframeworkcore")
-public class MyConfig {
-}
